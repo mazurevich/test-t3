50082a9e697af27db28d70da580bf5cfb4c6e604
Andrey_Mazurevich <mazyrevich@gmail.com>
2025-01-06 00:41:13 +0300

    added drizzle basic config

---
+++ src/db/index.ts
@@ -0,0 +1,20 @@
+import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema";
+
+config({ path: ".env" });
+
+const { env } = process;
+
+const connection = postgres(process.env.DATABASE_URL!, {
+  max: env.DB_MIGRATING || env.DB_SEEDING ? 1 : undefined,
+  onnotice: env.DB_SEEDING ? (notice) => console.log(notice) : undefined,
+});
+
+export const db = drizzle(connection, {
+  schema,
+  logger: true,
+});
+
+export type db = typeof db;
